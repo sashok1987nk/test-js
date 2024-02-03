29dfd515833f69e95a6aa08c95339af1aa26f2dc
sashok1987nik <sashok1987nk@ukr.net>
2024-02-03 11:40:09 +0100

    up

--- js/test-js.js
+++ js/test-js.js
@@ -1,5 +1,19 @@
-function greet(name) {
-    console.log(`Welcome ${name}!`);
+class Car {
+  static #maxPrice = 50000;
+  static checkPrice(price) {
+    if (price > Car.#maxPrice) {
+      return `Error! Price exceeds the maximum`;
+    } else {
+      return `Success! Price is within acceptable limits`;
+    }
   }
-  
- 
+
+  constructor({ price }) {
+    this.price = price;
+  }
+}
+
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
+
+console.log(Car.checkPrice(audi.price));
