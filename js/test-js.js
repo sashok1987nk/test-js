e9c389422420606ce5977586c158238cb4fb1f50
sashok1987nik <sashok1987nk@ukr.net>
2024-02-27 17:07:19 +0100

    up

--- js/test-js.js
+++ js/test-js.js
@@ -1,1 +1,9 @@
-console.log(document)
+const clickMe = document.querySelector(".js-click");
+const box = document.querySelector(".js-box");
+
+let mrgn = 0;
+
+function clickHandler(){
+    mrgn += 50;
+    box.style.margin = `${mrgn}px 0 0 ${mrgn}px`
+}
