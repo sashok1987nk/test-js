6f272554ab2695225fc918bda2c2fa49ff29df27
sashok1987nik <sashok1987nk@ukr.net>
2024-02-27 17:37:30 +0100

    up

--- js/test-js.js
+++
@@ -1,9 +0,0 @@
-const clickMe = document.querySelector(".js-click");
-const box = document.querySelector(".js-box");
-
-let mrgn = 0;
-
-function clickHandler(){
-    mrgn += 50;
-    box.style.margin = `${mrgn}px 0 0 ${mrgn}px`
-}
